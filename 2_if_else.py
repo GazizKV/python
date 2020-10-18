43d9d65ff54f14e4d78c48b97d44f5daced2d508
gazizkv <valitovgaziz@gmail.com>
2020-10-18 10:44:40 +0500

    else if

---
+++ 2_if_else.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+elif x > 0 and y < 0:
+    print(4)
+else:
+    print("Neve!")
